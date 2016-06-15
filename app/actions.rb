b17c9bb8514c6d35457a00d6b3433ab18b7ba92d
deemun <deepak.nayak93@gmail.com>
2016-06-14 18:10:47 -0700

    app commits

--- app/actions.rb
+++ app/actions.rb
@@ -13,6 +13,25 @@ get '/messages' do
 	erb :'messages/index'
 end
 
-get '/new' do
+
+post '/messages' do  #accepting the form data and updating the messages table in db
+	@message = Message.new(
+		title: params[:title], #params title comes from form. title on left is DB
+		content: params[:content],
+		author: params[:author]
+	)
+	if @message.save
+		redirect '/messages'
+	else
+		erb :'messages/new'
+	end
+end
+
+get '/messages/new' do
 	erb :'messages/new'
+end
+
+get '/messages/:id' do
+	@message = Message.find(params[:id])
+	erb :'messages/show'
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
 class Message < ActiveRecord::Base
 	
+	validates :title, presence: true
+
 end
